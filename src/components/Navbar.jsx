2223b30777e1725bedd982827d395b60d95590c2
Sahil <sk2003311@gmail.com>
2023-09-12 23:09:55 +0530

    fixed bug for theme icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -62,7 +62,7 @@ function Navbar() {
     
     <div className='backdrop-blur-lg z-10  w-[94%] tablet-lg:w-[32rem] laptop:w-[45rem] desktop:w-[55rem] bg-l-primary dark:bg-d-primary dark:bg-opacity-60 bg-opacity-60 flex outline outline-l-secondary dark:outline-d-secondary outline-[2px] rounded-full px-[24px] py-[16px] fixed laptop:bottom-[84%] bottom-[0rem] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <div className='basis-[30%] tablet-lg:basis-[40%] '>
-            <img onClick={handleTheme} className='cursor-pointer laptop:h-[20px] pt-[2px] dark:brightness-[50]' src={ theme === 'dark'? sunsvg : moonsvg} alt="" />
+            <img onClick={handleTheme} className='cursor-pointer h-[18px] laptop:h-[20px] pt-[2px] dark:brightness-[50]' src={ theme === 'dark'? sunsvg : moonsvg} alt="" />
         </div>
         <div className='flex gap-1 basis-[70%] tablet-lg:basis-[60%] font-mont text-[0.7rem] laptop:text-[1rem] font-medium justify-between'>
             <Link className=' hover:scale-105 active:scale-105 duration-150' onClick={scrollToTop} to="/">About</Link>
