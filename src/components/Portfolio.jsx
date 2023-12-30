99f2ad0c1143fca5476e677397ac0061b35f2fb4
Sahil <sk2003311@gmail.com>
2023-12-30 21:49:58 +0530

    added new function to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,16 +2,36 @@ import React from 'react'
 import Project from './Project'
 
 function Portfolio() {
+
+  const data = [
+    {
+      title: "Retro News",
+      img: "img1",
+      skills: ["React", "Tailwind CSS"],
+      link: "https://github.com/kmr-sahil/retro-news",
+      desc: "Reviving Nostalgia: A Modern Take on Retro Newspaper Layout – Seamlessly Fetching and Displaying News through an API.",
+    },
+    {
+      title: "Kolor Tool",
+      img: "img1",
+      skills: ["React", "Tailwind CSS", "Front-End"],
+      link: "https://github.com/kmr-sahil/retro-news",
+      desc: "Reviving Nostalgia: A Modern Take on Retro Newspaper Layout – Seamlessly Fetching and Displaying News through an API.",
+    },
+  ]
+
+  let i = 0;
+
   return (
     <div data-aos="fade" className='relative outline outline-l-secondary dark:outline-d-secondary outline-[2px] rounded-xl px-[1rem] mb-[6rem] laptop:basis-[70%] laptop:mt-[6rem]'>
         <div className='left-0 top-6 box-border font-mont font-bold text-[1rem] tablet-lg:text-[18px] bg-l-text dark:bg-d-accent absolute w-[8rem] rounded-sm border-r-[15px] border-r-l-primary dark:border-r-d-primary  border-t-[16px] border-b-[16px] border-t-l-text dark:border-t-d-accent border-b-l-text dark:border-b-d-accent'>
             <h1 className='absolute -top-3 left-2 text-l-primary dark:text-d-primary'>Portfolio</h1>
         </div>
         <div className='flex flex-col justify-start items-start pt-[5rem] pb-[1rem] gap-4'>
-        <Project link={'https://github.com/kmr-sahil/retro-news'} title={"Retro News"} img={"img1"} s1={"React"} s2={"Tailwind CSS"} s3={"Front-End"} para={"Reviving Nostalgia: A Modern Take on Retro Newspaper Layout – Seamlessly Fetching and Displaying News through an API."}></Project>
-        <Project link={'https://github.com/kmr-sahil/kolor-tool'} title={"Kolor Tool"} img={"img1"} s1={"HTML"} s2={"Tailwind CSS"} s3={"JavaScript"} para={"Effortlessly create harmonious color palettes with my innovative opacity-free shade generator website."}></Project>
-        <Project link={' https://kmr-sahil.github.io/dooze-redesign/'} title={"Dooze Redesign"} img={"img1"} s1={"HTML"} s2={"Tailwind CSS"} s3={"UI/UX"} para={"Elevated brand presence with a dynamic landing page redesign, crafting an immersive user experience at Dooze."}></Project>
-        <Project link={'https://github.com/kmr-sahil/todo-app'} title={"Todo Webapp"} img={"img1"} s1={"React"} s2={"CSS Modules"} s3={"JavaScript"} para={"Crafted a sleek Todo web app for seamless task management, tracking, and intuitive UI."}></Project>
+
+          {data.map((item, index) => (
+             <Project key={i++} data={item}/>
+          ))}
 
         </div>
 
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import img1 from "../assets/doo.png"
 
-function Project({title, para, s1, s2, s3, link, img}) {
+function Project({data}) {
+
+  let j = 0;
+
+  //const skills = data?.skills ? Object.values(data.skills) : [];
+  const skills = Object.values(data.skills);
     
   return (
-    <a href={link} target='_blank' className='self-stretch bg-l-secondary dark:bg-d-secondary rounded-lg flex gap-4 justify-start items-center px-[0.5rem] py-[0.25rem] tablet:py-[0.5rem] tablet:px-[1rem]'>
-        <img className='w-[64px] h-[64px] desktop:w-[80px] desktop:h-[80px] rounded-lg aspect-auto object-cover' src={img1} alt="img" />
+    <a href={data.link} target='_blank' className='self-stretch bg-l-secondary dark:bg-d-secondary rounded-lg flex gap-4 justify-start items-center px-[0.5rem] py-[0.25rem] tablet:py-[0.5rem] tablet:px-[1rem]'>
+        <img className='w-[64px] h-[64px] desktop:w-[80px] desktop:h-[80px] rounded-lg aspect-auto object-cover' src={data.img} alt="img" />
         <div className='font-over flex flex-col justify-between items-start flex-wrap py-[0.5rem]'>
-            <h3 className='font-medium text-[12px] desktop:text-[1rem]'>{title}</h3>
-            <p className='font-light text-[8px] tablet-lg:text-[10px] desktop:text-[12px] text-start leading-[0.625rem] desktop:leading-[1rem]'>{para}</p>
+            <h3 className='font-medium text-[12px] desktop:text-[1rem]'>{data.title}</h3>
+            <p className='font-light text-[8px] tablet-lg:text-[10px] desktop:text-[12px] text-start leading-[0.625rem] desktop:leading-[1rem]'>{data.desc}</p>
             <div className='text-[8px] font-extralight italic flex gap-[0.25rem] mt-1'>
-                <span className='bg-l-primary dark:bg-d-primary px-[0.5rem] py-[1px] rounded-sm'>{s1}</span>
-                <span className='bg-l-primary dark:bg-d-primary px-[0.5rem] py-[1px] rounded-sm'>{s2}</span>
-                <span className='bg-l-primary dark:bg-d-primary px-[0.5rem] py-[1px] rounded-sm'>{s3}</span>
+                  {skills.map((item, index) => (
+                    <span key={j++} className='bg-l-primary dark:bg-d-primary px-[0.5rem] py-[1px] rounded-sm'>{item}</span>
+                  ))}
+                
             </div>
         </div>
     </a>
