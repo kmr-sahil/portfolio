482f3e8c8e3aae6c8bbea220d2143724ca89f4e0
Sahil <sk2003311@gmail.com>
2024-01-01 19:51:28 +0530

    fix css

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -23,12 +23,12 @@ function Project({data}) {
   //const skills = Object.values(data.skills);
     
   return (
-    <a href={data?.link} target='_blank' className='self-stretch w-[48%] bg-l-secondary dark:bg-d-secondary rounded-lg flex flex-col gap-4 justify-start items-center px-[0.5rem] py-[0.25rem] tablet:py-[1rem] tablet:px-[1rem]'>
-        <img className='w-[64px] h-[64px] desktop:w-full desktop:h-[80px] rounded-lg aspect-auto object-cover' src={imageMap[data?.img]} alt="img" />
+    <a href={data?.link} target='_blank' className='self-stretch w-[100%] tablet-lg:w-[48%] h-auto bg-l-secondary dark:bg-d-secondary rounded-lg flex flex-col gap-4 justify-start items-center px-[0.5rem] py-[0.5rem] tablet:py-[1rem] tablet:px-[1rem]'>
+        <img className='h-[104px] w-full desktop:h-[80px] rounded-lg aspect-auto object-cover' src={imageMap[data?.img]} alt="img" />
         <div className='font-over flex flex-col justify-between items-start flex-wrap py-[0.5rem] gap-2'>
             <h3 className='font-medium text-[12px] desktop:text-[1rem]'>{data?.title}</h3>
             <p className='font-light text-[8px] tablet-lg:text-[10px] desktop:text-[12px] text-start leading-[0.625rem] desktop:leading-[1rem]'>{data?.desc}</p>
-            <div className='text-[12px] font-extralight italic flex gap-[0.25rem]'>
+            <div className='text-[10px] desktop:text-[12px] font-extralight italic flex flex-wrap gap-[0.25rem]'>
                   {skills.map((item, index) => (
                     <span key={j++} className='bg-l-primary dark:bg-d-primary px-[0.5rem] pt-[0.25rem] rounded-sm'>{item}</span>
                   ))}
